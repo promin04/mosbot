d9928085027922566a4fe879b9dc685d3a5f2871
Prommin <crazyfox_pr@hotmail.com>
2017-11-26 16:51:50 +0700

    bot

--- index.js
+++ index.js
@@ -1,64 +1,17 @@
 'use strict';
 
-// Imports dependencies and set up http server
-const
-  express = require('express'),
-  bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()); // creates express http server
 
-  // Adds support for GET requests to our webhook
-app.get('/webhook', (req, res) => {
+const BootBot = require('bootbot');
 
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
-
-  // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
-    }
-  }
+const bot = new BootBot({
+  accessToken: 'EAABqg0qlvU0BAPoptxIhuYZANwBjQ2e9YvZCwVkELRXZBtqBbpgO4Ol046dLunZCJdoJ4dYRT6jOXNupEDu1TuqsnOv5gUe7VJ9Wm2ZAYKE2b9hCO2iR2pgx5m2XZBNbD89ZA4LjaJJWEpbdeZB2nORixsjZBR7vL1pqB2jO5ZCkUPoHZBH9YhvZBA7w',
+  verifyToken: '<YOUR_VERIFY_TOKEN>',
+  appSecret: '363f02fde9996332fd95a5859babf05d'
 });
 
-  // Creates the endpoint for our webhook
-app.post('/webhook', (req, res) => {
-
-  let body = req.body;
-  console.log('body',JSON.stringify(body));
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
-
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
-      let webhookEvent = entry.messaging[0];
-      console.log(webhookEvent);
-    });
-
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-
+bot.on('message', (payload, chat) => {
+  const text = payload.message.text;
+  chat.say(`Echo: ${text}`);
 });
 
-// Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+bot.start();
